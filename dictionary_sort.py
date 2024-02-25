538319bfd7ff94af2fdf985d5e89b7e69550fac9
venkatchidipudi2018 <35222449+venkatchidipudi2018@users.noreply.github.com>
2024-02-25 16:01:57 +0530

    Create dictionary_sort.py

---
+++ dictionary_sort.py
@@ -0,0 +1,13 @@
+a = [
+			{'address': '5412 N CLARK', 'date': '07/01/2012'},
+			{'address': '5148 N CLARK', 'date': '07/04/2012'},
+			{'address': '5800 E 58TH', 'date': '07/02/2012'},
+			{'address': '2122 N CLARK', 'date': '07/03/2012'},
+			{'address': '5645 N RAVENSWOOD', 'date': '07/02/2012'},
+			{'address': '1060 W ADDISON', 'date': '07/02/2012'},
+			{'address': '4801 N BROADWAY', 'date': '07/01/2012'},
+			{'address': '1039 W GRANVILLE', 'date': '07/04/2012'}
+]
+
+a.sort(key=lambda x: x["date"])
+print (a)
